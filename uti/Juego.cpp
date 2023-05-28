d61adcb372ebce0e4679b814269c218275d39483
CumFlakes <a190082@lazarocardenas.edu.mx>
2023-05-28 12:54:16 -0700

    Actualizar clase Juego

--- uti/Juego.cpp
+++ uti/Juego.cpp
@@ -1,1 +1,21 @@
 #include "Juego.h"
+
+void Juego::initWindow()
+{
+}
+
+void Juego::initGUI()
+{
+}
+
+void Juego::initSubmarino()
+{
+}
+
+Juego::Juego()
+{
+}
+
+Juego::~Juego()
+{
+}
--- uti/Juego.h
+++ uti/Juego.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "submarino.hpp"
+#include "Enemigo.h"
+
+#include <vector>
+
 /*
 	Clase que actua como motor de juego. 
 	- Aqui se actualiza y renderiza todo.
@@ -7,5 +12,32 @@
 
 class Juego
 {
+private:
+	// Ventana de juego
+	sf::RenderWindow* window;
+
+	// Objeto submarino (jugador)
+	Submarino* submarino;
+
+	// Enemigos
+	std::vector<Enemigo> enemigos;
+
+	// Interfaz de usuario (GUI)
+	sf::RectangleShape barraVidaMax;
+	sf::RectangleShape barraVida;
+
+	sf::Font fuente;
+	sf::Text puntos;
+	sf::Text gameOver;
+
+	// Funciones Init
+	void initWindow();
+	void initGUI();
+
+	void initSubmarino();
+public:
+	Juego();
+	~Juego();
+
 };
 
