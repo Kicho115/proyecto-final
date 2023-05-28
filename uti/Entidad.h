398d83aff29d998382a29a8df8a5772e04d0787d
CumFlakes <a190082@lazarocardenas.edu.mx>
2023-05-28 01:23:57 -0700

    Agregar variables a clase Entidad

--- uti/Entidad.h
+++ uti/Entidad.h
@@ -1,5 +1,13 @@
 #pragma once
 class Entidad
 {
+protected:
+	int vida;
+	int vidaMax;
+	int ataque;
+	int puntos;
+	float velocidad;
+public:
+	//void setPosicion(const float x, const float y);
 };
 
