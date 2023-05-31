8d87ed615db2683ad0c0da864b42779c162c35d2
CumFlakes <a190082@lazarocardenas.edu.mx>
2023-05-30 18:21:40 -0700

    Actualizar clase Armas

--- uti/Armas.cpp
+++ uti/Armas.cpp
@@ -1,1 +1,30 @@
 #include "Armas.h"
+
+Armas::Armas(sf::Texture* textura, float posX, float posY, float dirX, float dirY, float velocidad)
+{
+	sprite.setTexture(*textura);
+	sprite.setPosition(posX, posY);
+	direccion.x = dirX;
+	direccion.y = dirY;
+	this->velocidad = velocidad;
+
+}
+
+Armas::~Armas()
+{
+}
+
+sf::FloatRect Armas::getBounds() const
+{
+	return sprite.getGlobalBounds();
+}
+
+void Armas::movimiento()
+{
+	sprite.move(direccion * velocidad);
+}
+
+void Armas::render(sf::RenderTarget* target)
+{
+	target->draw(sprite);
+}
--- uti/Armas.h
+++ uti/Armas.h
@@ -1,5 +1,24 @@
 #pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
 class Armas
 {
+private:
+	float velocidad;
+
+	sf::Sprite sprite;
+	sf::Vector2f direccion;
+public:
+	Armas(sf::Texture* textura, float posX, float posY, float dirX, float dirY, float velocidad);
+	~Armas();
+
+	// Accesores
+	sf::FloatRect getBounds() const;
+
+	// Funciones
+	void movimiento();
+	void render(sf::RenderTarget* target);
 };
 
